10f05a8151a2f17cfaf7164c1ab1a937c2a5d7e3
Andy Hu <huandy01@gmail.com>
2022-04-24 14:15:24 +0200

    fixes

--- raf/src/components/QuickLinks/QuickLinks.js
+++ raf/src/components/QuickLinks/QuickLinks.js
@@ -6,13 +6,13 @@ export default function QuickLinks() {
   
     return (
     <div className='containerQuickLinks' id="quickLinks">
-        <div>
+        <div className='divQuickLinks'>
             <p className='UIHeadline'>QUICK LINKS</p>
             <ul>
                 <li className='listElement' onClick={() => {window.open("https://hulongofficial.de/", "_blank")}}>Entwickelt von Hulong RR</li>
             </ul>
         </div>
-        <div>
+        <div className='divSocial'>
             <p className='UIHeadline'>SOCIAL</p>
             <ul className='socialDiv'>
                 <li className='socialElement' onClick={() => {window.open("https://facebook.com/RAFCamora.official", "_blank")}}><FacebookIcon sx={{ fontSize: "calc(1.5rem + 1vw)" }}/></li>
